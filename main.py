867653babcdca5743d0547d37fccfdf1375fa317
Mahan Fathi <mahan0fathi@gmail.com>
2021-12-17 12:53:48 -0500

    Make changes to deconvs and hyperparameters

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from byol_pytorch.mayo_pytorch import MAYO
 from torch.utils.data import DataLoader
 import torchvision
 from torchvision import models
-
 from torchvision import transforms as T
+from matplotlib import pyplot as plt
 
 
 resnet = models.resnet18(pretrained=True)
@@ -12,7 +12,9 @@ resnet = models.resnet18(pretrained=True)
 learner = MAYO(
     resnet,
     image_size = 128,
-    hidden_layer = -3
+    hidden_layer = -3,
+    eta=100,
+    moving_average_decay=0.99,
 )
 
 opt = torch.optim.Adam(learner.parameters(), lr=3e-4)
@@ -22,9 +24,10 @@ stl10 = torchvision.datasets.STL10(
     download=True,
     transform = T.Compose([T.ToTensor()]),
 )
-train_dataloader = DataLoader(stl10, batch_size=64, shuffle=True)
+train_dataloader = DataLoader(stl10, batch_size=128, shuffle=True)
 
 learner.calc_norm_values(stl10.data)
+
 
 for _ in range(1000):
     for images, _ in train_dataloader:
@@ -35,6 +38,7 @@ for _ in range(1000):
         print(loss)
         opt.step()
         # learner.update_moving_average() # update moving average of target encoder
+
 
 # save your improved network
 torch.save(resnet.state_dict(), './improved-net.pt')
